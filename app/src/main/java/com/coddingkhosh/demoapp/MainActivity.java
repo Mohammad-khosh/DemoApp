e2716dfe2c25ac8d16e76cc4aae6927465aeddb3
Mohammad <mohammadkhosh4640@gmail.com>
2021-10-16 22:20:35 +0330

    custom progress

--- app/src/main/java/com/coddingkhosh/demoapp/MainActivity.java
+++ app/src/main/java/com/coddingkhosh/demoapp/MainActivity.java
@@ -4,30 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
-    private int progr = 0;
+   // private int progr = 0;
+    private TextView textView;
+    private static final String TAG = "TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         progressInit();
+        textView = findViewById(R.id.materialTextView);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-               progressBar.setProgress(progr);
-               progr++;
+                Log.d(TAG, "run: ");
+              textView.setText(String.valueOf(progressBar.getProgress()));
             }
-        },6000);
+        },100);
 
     }
 
     private void progressInit(){
-     progressBar = new ProgressBar(this);
+     progressBar = findViewById(R.id.progress_bar);
+   //  progressBar.setProgress(progr);
     }
 }
