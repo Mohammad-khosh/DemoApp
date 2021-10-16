596be78dc7d522f102b777ef12dc273a99744a91
Mohammad <mohammadkhosh4640@gmail.com>
2021-10-16 21:41:13 +0330

    custom progress

--- app/src/main/java/com/coddingkhosh/demoapp/MainActivity.java
+++ app/src/main/java/com/coddingkhosh/demoapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.coddingkhosh.demoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
+    private int progr = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         progressInit();
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               progressBar.setProgress(progr);
+               progr++;
+            }
+        },6000);
 
     }
 
